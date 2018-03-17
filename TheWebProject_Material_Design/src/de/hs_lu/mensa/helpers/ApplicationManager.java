6f58e878d19c5a29cb9d817a9df3d715f7820b8d
loicy <loicy@Loic-Yondjeu.localdomain>
2018-03-17 16:07:42 +0100

    introducing the new haitem avatar :)

--- TheWebProject_Material_Design/src/de/hs_lu/mensa/helpers/ApplicationManager.java
+++ TheWebProject_Material_Design/src/de/hs_lu/mensa/helpers/ApplicationManager.java
@@ -11,38 +11,43 @@ import de.hs_lu.mensa.model.MealPlan;
 public abstract class ApplicationManager {
 	
 	 public static void setUpTodayMeals(ServletContext application) throws ParseException{
-		  Date today = DateHelper.getDateTimeRemoved(new Date());
-		  
-		  MealPlan todayMealPlan = new MealPlan();
-		  todayMealPlan.setDate(today);
-		  
-		  todayMealPlan.mongoRead();
-		  
-		  //Für den Debug
-		  System.out.println("Die IDs der Speise für heute sind:" + todayMealPlan.getRelatedMeals_ids().toString());
-		  
-		  if(todayMealPlan.getRelatedMeals_ids().size() >= 2){
-			  Meal todayMealVegetarian = new Meal();
-			  todayMealVegetarian.setMeal_id(todayMealPlan.getRelatedMeals_ids().get(0));
+		  try {
+			Date today = DateHelper.getDateTimeRemoved(new Date());
 			  
-			  todayMealVegetarian.mongoReadById();
+			  MealPlan todayMealPlan = new MealPlan();
+			  todayMealPlan.setDate(today);
 			  
-			  Meal todayMealVegetarianTemp = (Meal)application.getAttribute("todayMealVegetarian");
-			  if(todayMealVegetarianTemp == null){
-				  application.setAttribute("todayMealVegetarian", todayMealVegetarian);
+			  todayMealPlan.mongoRead();
+			  
+			  //Für den Debug
+			  System.out.println("Die IDs der Speise für heute sind:" + todayMealPlan.getRelatedMeals_ids().toString());
+			  
+			  if(todayMealPlan.getRelatedMeals_ids().size() >= 2){
+				  Meal todayMealVegetarian = new Meal();
+				  todayMealVegetarian.setMeal_id(todayMealPlan.getRelatedMeals_ids().get(0));
+				  
+				  todayMealVegetarian.mongoReadById();
+				  
+				  Meal todayMealVegetarianTemp = (Meal)application.getAttribute("todayMealVegetarian");
+				  if(todayMealVegetarianTemp == null){
+					  application.setAttribute("todayMealVegetarian", todayMealVegetarian);
+				  }
+				  
+				  
+				  Meal todayMealVollkost = new Meal();
+				  todayMealVollkost.setMeal_id(todayMealPlan.getRelatedMeals_ids().get(1));
+				  
+				  todayMealVollkost.mongoReadById();
+				  
+				  Meal todayMealVollkostTemp = (Meal)application.getAttribute("todayMealVollkost");
+				  if(todayMealVollkostTemp == null){
+					  application.setAttribute("todayMealVollkost", todayMealVollkost);
+				  }
 			  }
-			  
-			  
-			  Meal todayMealVollkost = new Meal();
-			  todayMealVollkost.setMeal_id(todayMealPlan.getRelatedMeals_ids().get(1));
-			  
-			  todayMealVollkost.mongoReadById();
-			  
-			  Meal todayMealVollkostTemp = (Meal)application.getAttribute("todayMealVollkost");
-			  if(todayMealVollkostTemp == null){
-				  application.setAttribute("todayMealVollkost", todayMealVollkost);
-			  }
-		  }
+		} catch (Exception e) {
+			//Das Laden der heutigen Speisen ist fehlgeschlagen
+			e.printStackTrace();
+		}
 		  
 	 }
 
